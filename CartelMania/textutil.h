800b485f2c9e3fc0a2d0c330760e86087516734e
Hernán Di Pietro <indiocolifa@hotmail.com>
2017-04-06 01:50:53 -0300

    FindFontToFillTextInRect calculates and (optionally) returns font size in Em.

--- CartelMania/textutil.h
+++ CartelMania/textutil.h
@@ -7,21 +7,25 @@
 inline std::unique_ptr<Gdiplus::Font> FindFontToFillTextInRect(
 	const Gdiplus::Graphics& gr,
 	const Gdiplus::RectF& rc,
-	const std::wstring& fontFamily, 
+	const Gdiplus::FontFamily& fontFamily,
 	const std::wstring& text,
-	const Gdiplus::StringFormat& format)
+	const Gdiplus::StringFormat& format,
+	OUT Gdiplus::REAL* outFontSize = nullptr)
 {
 	// This iterative approximation method is a *total Hack* 
 	// better solution? 
-	
+
 	Gdiplus::RectF boundingBox{};
 	Gdiplus::REAL fontSize = 10.0f;
 	while (boundingBox.Height < rc.Height && boundingBox.Width < rc.Width)
 	{
 		fontSize += 1.0f;
-		Gdiplus::Font tryFont(&Gdiplus::FontFamily(fontFamily.c_str()), fontSize, 0,Gdiplus::Unit::UnitPixel);
+		Gdiplus::Font tryFont(&fontFamily, fontSize, 0, Gdiplus::Unit::UnitPoint);
 		gr.MeasureString(text.c_str(), -1, &tryFont, rc, &format, &boundingBox);
 	}
 
-	return std::make_unique<Gdiplus::Font>(&Gdiplus::FontFamily(fontFamily.c_str()), fontSize, 0, Gdiplus::Unit::UnitPixel);
+	if (outFontSize)
+		*outFontSize = fontSize;
+
+	return std::make_unique<Gdiplus::Font>(&fontFamily, fontSize, 0, Gdiplus::Unit::UnitPoint);
 }
