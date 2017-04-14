b6b11564b543ea2f4261b21f80970a14390e98eb
Hernán Di Pietro <indiocolifa@hotmail.com>
2017-04-14 18:54:27 -0300

    Testing subdivision algorithm in renderer

--- CartelMania/textfx_solid.cxx
+++ CartelMania/textfx_solid.cxx
@@ -6,6 +6,7 @@
 #include "banner.h"
 #include "colors.h"
 #include "Debug.h"
+#include "geom.h"
 
 using namespace Gdiplus;
 using namespace std;
@@ -59,6 +60,105 @@ void TextFxSolid::DrawLine(const BannerLine& line, Graphics& gr, const RectF& li
 	mtx.Translate(-lineRect.Width / 2.0f, -lineRect.Height / 2.0f);
 	path.Transform(&mtx);
 
-	gr.FillPath(&brush, &path);
-	gr.DrawPath(&pen, &path);
+	path.Flatten();
+
+	PathData pathData;
+	path.GetPathData(&pathData);
+	Gdiplus::GraphicsPath newPath;
+
+	////////-------
+	Gdiplus::GraphicsPathIterator pathIter(&path);
+	pathIter.Rewind();
+	dprintf(L"String Path subpath count=%d\n", pathIter.GetSubpathCount());
+	
+	for (int i = 0; i < pathIter.GetSubpathCount(); ++i)
+	{
+		Gdiplus::GraphicsPath charPath;
+		BOOL isClosed;
+		int dataSize = pathIter.NextSubpath(&charPath, &isClosed);
+
+		dprintf(L">> subpath [%d], closed=%d, data_points=%d\n", i, isClosed, dataSize);
+
+		for (int j = 0; j < dataSize - 1; j++)
+		{
+			const REAL px0 = pathData.Points[j].X;
+			const REAL py0 = pathData.Points[j].Y;
+			const REAL px1 = pathData.Points[j + 1].X;
+			const REAL py1 = pathData.Points[j + 1].Y;
+			//const REAL dist = sqrtf((px1 - px0)*(px1 - px0) + (py1 - py0)*(py1 - py0));
+
+			SubdivideLineAddTo(px0, py0, px1, py1, 2, newPath, true, &gr);
+		}
+
+		// Close path
+
+		SubdivideLineAddTo(pathData.Points[dataSize-1].X, pathData.Points[dataSize-1].Y, 
+			pathData.Points[0].X, pathData.Points[0].Y, 2, newPath, true, &gr);
+	}
+
+
+	//GraphicsPath* charPath = nullptr;
+	//BOOL isClosed;
+
+
+	//while (pathIter.NextSubpath(&path, &isClosed) > 0)
+	//{
+	//	XASSERT(isClosed); 
+	//	PathData pathData;
+	//	charPath->GetPathData(&pathData);
+
+	//	dprintf(L"Path point# =%d\n", pathData.Count);
+	//}*/
+	//
+	//dprintf(L"Path point# =%d\n", pathData.Count);
+	//for (int i = 0; i < pathData.Count; ++i)
+	//{
+	//	const REAL px0 = pathData.Points[i].X;
+	//	const REAL py0 = pathData.Points[i].Y;
+	//	const REAL px1 = pathData.Points[i+1].X;
+	//	const REAL py1 = pathData.Points[i+1].Y;
+	//	const REAL dist = sqrtf((px1 - px0)*(px1 - px0) + (py1 - py0)*(py1 - py0));
+	//	
+	//	const REAL  SUBDIV = 2.0f;
+	//	REAL px = px0, py = py1;		
+
+	//	// Cx = Ax * (1 - t) + Bx * t
+	//	// Cy = Ay * (1 - t) + By * t
+
+	//	dprintf(L"(PX0=%.f PY0=%.f)-(PX1=%.f PY1=%.f) \n", px0, py0, px1, py1);
+
+	//	for (int j = 0; j < (int)SUBDIV ; j++)
+	//	{
+	//		const REAL t = j / SUBDIV;
+	//		REAL nx = px0 * (1 - t) + px1 * t;
+	//		REAL ny = py0 * (1 - t) + py1 * t;
+	//		newPath.AddLine(px, py, nx, ny);
+	//		gr.DrawLine(&Pen(MakeRandColor(), 2), px, py, nx, ny);
+	//		px = nx;
+	//		py = ny;
+	//	}
+
+	//	if (pathData.Types[i+1] & 0x80)
+	//	{
+	//		// End  of shape
+	//		newPath.AddLine(px, py, px0, py0);
+	//		gr.DrawLine(&Pen(MakeRandColor(), 2), px, py, px0, py0);
+	//		gr.FillEllipse(&SolidBrush(Gdiplus::Color::Blue), pathData.Points[i].X - 5, pathData.Points[i].Y - 5, 10.0f, 10.0f);
+	//	}
+	//	else
+	//	{
+	//		newPath.AddLine(px, py, px1, py1);
+	//		gr.DrawLine(&Pen(MakeRandColor(), 2), px, py, px1, py1);
+	//	}
+
+	//	//else
+	//	//{
+	//	//	gr.DrawEllipse(&Pen(Gdiplus::Color::Red, 2), pathData.Points[i].X - 5, pathData.Points[i].Y - 5, 10.0f, 10.0f);
+	//	//	gr.DrawLine(&Pen(Gdiplus::Color::Green, 2), pathData.Points[i], pathData.Points[i + 1]);
+	//	//}
+
+	//}
+
+	//gr.FillPath(&brush, &newPath);
+	//gr.DrawPath(&pen, &newPath);
 }
