67816bbb1fbc4fa0f4d1875df2ad1f4131bde9da
Hernán Di Pietro <indiocolifa@hotmail.com>
2017-04-18 11:38:04 -0300

    added dprintf function

--- CartelMania/debug.h
+++ CartelMania/debug.h
@@ -14,5 +14,21 @@ inline void _FatalMsg(LPCSTR fmt, ...)
 	va_end(args);
 
 	MessageBoxA(0, szBuffer, "Fatal Error", MB_ICONERROR | MB_OK);
+
+	DebugBreak();
 }
 
+inline void dprintf(LPCWSTR fmt, ...)
+{
+#ifdef _DEBUG
+	const int MAX_MESSAGE = 1024;
+	va_list args;
+	va_start(args, fmt);
+
+	WCHAR szBuffer[MAX_MESSAGE] = { 0 };
+	StringCchVPrintfW(szBuffer, MAX_MESSAGE, fmt, args);
+	va_end(args);
+
+	OutputDebugStringW(szBuffer);
+#endif
+}
